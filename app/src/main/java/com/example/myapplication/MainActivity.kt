8556d4d8a9a0990253e98429c83d93ab98c774f1
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2022-07-13 15:22:08 -0300

    Imagem Adicionada

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.view.View
 import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var textBTG = findViewById<View>(R.id.textTop)
+      //  var textJames = findViewById<View>(R.id.textTop)
+        var buttonC = findViewById<Button>(R.id.button)
+
+        buttonC.setOnClickListener{
+          //  textJames = "Grupo de cinco galáxias que fica na constelação de Pegasus, a cerca de 290 milhões anos-luz da Terra"
+        }
+
     }
 }
