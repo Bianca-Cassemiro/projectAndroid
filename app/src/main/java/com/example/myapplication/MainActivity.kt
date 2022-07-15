eeb80ea545fb854614eeeea57087f32c04868f9a
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2022-07-14 23:24:45 -0300

    Alterações na imagem

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
+
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.myapplication.databinding.ActivityMainBinding
 import org.w3c.dom.Text
 
+
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("WrongViewCast")
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-      //  var textJames = findViewById<View>(R.id.textTop)
+        var textJames: TextView = findViewById(R.id.textTop)
         var buttonC = findViewById<Button>(R.id.button)
+        var imageCarina = findViewById<ImageView>(R.id.imageCarina)
+        var uri = "@drawable/_5712498"
 
         buttonC.setOnClickListener{
-          //  textJames = "Grupo de cinco galáxias que fica na constelação de Pegasus, a cerca de 290 milhões anos-luz da Terra"
+
+            textJames.setText("Quinteto de Stephan, grupo de cinco galáxias que fica na constelação de Pegasus, a cerca de 290 milhões anos-luz da Terra")
+            buttonC.text = "Legal né"
+            val image = resources.getDrawable(R.drawable._5712498)
+
+           imageCarina.setImageDrawable(image)
+
         }
 
     }
