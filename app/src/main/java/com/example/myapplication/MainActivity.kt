3412fa0126442c53efdba8da102611a1cda3229c
biankk <99203402+Bianca-Cassemiro@users.noreply.github.com>
2022-07-14 23:55:00 -0300

    Evolui o findViewById para ViewBinding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,25 +13,21 @@ import org.w3c.dom.Text
 
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        var textJames: TextView = findViewById(R.id.textTop)
-        var buttonC = findViewById<Button>(R.id.button)
-        var imageCarina = findViewById<ImageView>(R.id.imageCarina)
-        var uri = "@drawable/_5712498"
 
-        buttonC.setOnClickListener{
-
-            textJames.setText("Quinteto de Stephan, grupo de cinco galáxias que fica na constelação de Pegasus, a cerca de 290 milhões anos-luz da Terra")
-            buttonC.text = "Legal né"
+        binding.button.setOnClickListener{
+            binding.textTop.setText("Quinteto de Stephan, grupo de cinco galáxias que fica na constelação de Pegasus, a cerca de 290 milhões anos-luz da Terra")
+            binding.button.text = "Legal né"
             val image = resources.getDrawable(R.drawable._5712498)
 
-           imageCarina.setImageDrawable(image)
-
+           binding.imageCarina.setImageDrawable(image)
         }
 
     }
